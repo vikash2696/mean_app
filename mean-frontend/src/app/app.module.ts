a56ddb2726a60026b907404fb6ade8d3d9d97e37
vikash2696 <vikash2696@gmail.com>
2017-12-10 22:50:42 +0530

    shared added

--- mean-frontend/src/app/app.module.ts
+++ mean-frontend/src/app/app.module.ts
@@ -4,23 +4,36 @@ import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
 import { HttpClientModule } from "@angular/common/http";
 import { HttpModule } from '@angular/http';
-import {ReactiveFormsModule} from '@angular/forms';
+import { ReactiveFormsModule} from '@angular/forms';
 import { FormsModule } from '@angular/forms';
 import { DataTableModule } from 'primeng/primeng';
 import { UserService } from './services/user.service';
+import { RouterModule , Routes}  from '@angular/router';
+import { NavComponent } from './shared/nav/nav.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
+
+const routes : Routes  = [{
+  path:'users',
+  component : UserComponent
+}];
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserComponent
+    UserComponent,
+    NavComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
     HttpModule,
     HttpClientModule,
     ReactiveFormsModule,
-    FormsModule,DataTableModule
+    FormsModule,DataTableModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [UserService],
   bootstrap: [AppComponent]
